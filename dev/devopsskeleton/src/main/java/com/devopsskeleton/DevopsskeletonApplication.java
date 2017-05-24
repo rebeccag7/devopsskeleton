8a3dbc9ab9ee6930262fa2137b0c72bd73abc8fb
Rebecca Goveia <rgoveia@asu.edu>
2017-05-23 23:21:00 -0700

    Initial commit for first spring project

---
+++ dev/devopsskeleton/src/main/java/com/devopsskeleton/DevopsskeletonApplication.java
@@ -0,0 +1,12 @@
+package com.devopsskeleton;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevopsskeletonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DevopsskeletonApplication.class, args);
+	}
+}
---
+++ dev/devopsskeleton/src/test/java/com/devopsskeleton/DevopsskeletonApplicationTests.java
@@ -0,0 +1,16 @@
+package com.devopsskeleton;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class DevopsskeletonApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
